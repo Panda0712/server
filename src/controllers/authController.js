e00511337bc65ca7254f264bb6cce051e8bdbf04
Pham Tuan <tuan.pham.super@gmail.com>
2024-12-03 16:28:41 +0700

    signup user in backend

---
+++ src/controllers/authController.js
@@ -0,0 +1,51 @@
+const UserModel = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+
+const getJsonWebtoken = async (email, id) => {
+  const payload = {
+    email,
+    id,
+  };
+
+  const token = jwt.sign(payload, process.env.SECRET_KEY, {
+    expiresIn: "7d",
+  });
+
+  return token;
+};
+
+const register = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const existingUser = await UserModel.findOne({ email });
+
+  if (existingUser) {
+    res.status(401);
+    throw new Error("User has already been registered");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new UserModel({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  await newUser.save();
+
+  res.status(200).json({
+    message: "Register successfully",
+    data: {
+      ...newUser,
+      accessToken: await getJsonWebtoken(email, newUser.id),
+    },
+  });
+});
+
+module.exports = {
+  register,
+};
